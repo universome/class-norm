8822055500dffc00265b524af94985885bc3df28
Ivan Skorokhodov <iskorokhodov@gmail.com>
2019-10-14 17:03:17 +0300

    feat: gradient penalty for WGAN

---
+++ src/utils/losses.py
@@ -0,0 +1,23 @@
+import torch
+from torch import Tensor, autograd
+
+def compute_gradient_penalty(discriminator, x_real, x_fake) -> Tensor:
+    """
+    Computes gradient penalty according to WGAN-GP paper
+    """
+    assert x_real.ndim == 2, "If you want to use other input sizes, implement interpolations for them first."
+    assert x_real.size() == x_fake.size()
+
+    eps = torch.rand(x_real.size(0), 1).to(x_real.device)
+    interpolations = eps * x_real + (1 - eps) * x_fake
+    interpolations.requires_grad_(True)
+    outputs = discriminator(interpolations)
+
+    grads = autograd.grad(
+        outputs=outputs,
+        inputs=interpolations,
+        grad_outputs=torch.ones(outputs.size()),
+        retain_graph=True, create_graph=True, only_inputs=True
+    )[0]
+
+    return ((grads.norm(2, dim=1) - 1) ** 2).mean()
