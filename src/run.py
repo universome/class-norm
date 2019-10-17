8406ba14d3a031133c8b7ccfbfcc6b17566a3abc
Ivan Skorokhodov <iskorokhodov@gmail.com>
2019-10-17 14:40:45 +0300

    feat: track AUSUC scores

--- src/run.py
+++ src/run.py
@@ -14,6 +14,7 @@ def run_trainer(args):
     config = Config.load(f'configs/{args.config_name}.yml')
     config.set('experiments_dir', 'experiments')
     config.set('exp_name', args.config_name)
+    config.set('random_seed', args.random_seed)
 
     for run_idx in range(args.num_runs):
         print(f'\n<======= RUN # {run_idx} =======>\n')
@@ -43,7 +44,7 @@ def get_hpo_configs(config: Config) -> List[Config]:
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('Running LLL trainer')
-    #parser.add_argument('-s', '--seed', default=42, help='Random seed to fix')
+    parser.add_argument('-s', '--random_seed', default=42, type=int, help='Random seed to fix')
     parser.add_argument('-c', '--config_name', type=str, default='basic_cub', help='Which config to run?')
     parser.add_argument('-n', '--num_runs', type=int, default=1, help='How many times we should run the experiment?')
     args = parser.parse_args()
