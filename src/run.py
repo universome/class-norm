c944d8a6acbb13b5e5b6af96608d18f81d19f4d4
Ivan Skorokhodov <iskorokhodov@gmail.com>
2019-10-22 14:37:35 +0300

    feat: pass config options from CLI

--- src/run.py
+++ src/run.py
@@ -1,7 +1,6 @@
-from os import PathLike
 import argparse
 import sys; sys.path.append('.')
-from typing import List
+from typing import List, Dict, Any
 
 import numpy as np
 from firelab.config import Config
@@ -11,8 +10,14 @@ from src.trainers.lll_trainer import LLLTrainer
 from src.utils.metrics import compute_average_accuracy
 
 
-def run_trainer(args):
-    config = load_config(args.config_name, args)
+CONFIG_ARG_PREFIX = '--config.'
+
+
+def run_trainer(args: argparse.Namespace, config_cli_args: List[str]):
+    config = load_config(args, config_cli_args)
+
+    print('Config:')
+    print(config)
 
     for run_idx in range(args.num_runs):
         print(f'\n<======= RUN # {run_idx} =======>\n')
@@ -35,7 +40,7 @@ def run_trainer(args):
             LLLTrainer(config).start()
 
 
-def load_config(config_path: PathLike, args: argparse.Namespace) -> Config:
+def load_config(args: argparse.Namespace, config_cli_args: List[str]) -> Config:
     base_config = Config.load('configs/base.yml')
     config = Config.load(f'configs/{args.config_name}.yml')
 
@@ -51,7 +56,58 @@ def load_config(config_path: PathLike, args: argparse.Namespace) -> Config:
     hp = config.hp.overwrite(config.hp_for_datasets.get(args.dataset))
     config = config.overwrite(Config({'hp': hp.to_dict()}))
 
+    # Overwriting with CLI arguments
+    config_cli_args: Dict = process_cli_config_args(config_cli_args)
+    config = config.overwrite(Config(config_cli_args))
+
     return config
+
+
+def process_cli_config_args(config_args:List[str]) -> Dict:
+    """Takes config args from the CLI and converts them to a dict"""
+    # assert len(config_args) % 3 == 0, \
+    #     "You should pass config args in [--config.arg_name arg_value arg_type] format"
+    assert len(config_args) % 2 == 0, \
+        "You should pass config args in [--config.arg_name arg_value] format"
+    arg_names = [config_args[i] for i in range(0, len(config_args), 2)]
+    arg_values = [config_args[i] for i in range(1, len(config_args), 2)]
+
+    result = {}
+
+    for name, value in zip(arg_names, arg_values):
+        assert name.startswith(CONFIG_ARG_PREFIX), \
+            f"Argument {name} is unkown and does not start with `config.` prefix. Cannot parse it."
+
+        result[name[len(CONFIG_ARG_PREFIX):]] = infer_type_and_convert(value)
+
+    return result
+
+
+def infer_type_and_convert(value:str) -> Any:
+    """
+    Chances are high that this function should never exist...
+    It tries to get a proper type and converts the value to it.
+    """
+    if value.lower() == 'true':
+        return True
+    elif value.lower() == 'false':
+        return False
+    elif value.isdigit():
+        return int(value)
+    elif is_float(value):
+        return float(value)
+    else:
+        return value
+
+
+def is_float(value:Any) -> bool:
+    """One more dirty function: it checks if the string is float."""
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
+
 
 
 def get_hpo_configs(config: Config) -> List[Config]:
@@ -64,6 +120,7 @@ if __name__ == '__main__':
     parser.add_argument('-s', '--random_seed', default=42, type=int, help='Random seed to fix')
     parser.add_argument('-c', '--config_name', type=str, default='basic', help='Which config to run?')
     parser.add_argument('-n', '--num_runs', type=int, default=1, help='How many times we should run the experiment?')
-    args = parser.parse_args()
 
-    run_trainer(args)
+    args, config_args = parser.parse_known_args()
+
+    run_trainer(args, config_args)
