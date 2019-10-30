50a07cbb35b605fa68df1d6dbcc2ef2dfcc118d5
Ivan Skorokhodov <iskorokhodov@gmail.com>
2019-10-30 13:08:47 +0300

    feat: create slurm job

---
+++ slurm/slurm_job.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+#SBATCH --ntasks 1
+#SBATCH --time 10080
+#SBATCH --gres gpu:1
+#SBATCH --cpus-per-task 4
+#SBATCH -o slurm_output/output-%a.out
+
+cd /users/skoroki/zslll
+python src/run.py $cli_args
