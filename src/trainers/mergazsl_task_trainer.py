eed511e60ab00edf6ce1605db4dbe471a4574d01
Ivan Skorokhodov <iskorokhodov@gmail.com>
2019-10-31 15:25:41 +0300

    fix: classifier was not training :|

--- src/trainers/mergazsl_task_trainer.py
+++ src/trainers/mergazsl_task_trainer.py
@@ -1,5 +1,4 @@
 import os
-import random
 from copy import deepcopy
 from typing import Tuple
 
@@ -7,6 +6,7 @@ import torch
 from torch import Tensor
 import numpy as np
 from torch.utils.tensorboard import SummaryWriter
+from torch.nn.utils import clip_grad_norm_
 
 from src.trainers.task_trainer import TaskTrainer
 from src.utils.lll import prune_logits
@@ -22,7 +22,8 @@ class MeRGAZSLTaskTrainer(TaskTrainer):
         self.class_centroids = compute_class_centroids(self.task_ds_train, self.config.data.num_classes)
         self.prev_model = deepcopy(self.model).to(self.device_name).eval()
         self.current_classes = self.main_trainer.class_splits[self.task_idx]
-        self.previously_seen_classes = list(set([c for task_id in range(self.task_idx) for c in self.main_trainer.class_splits[task_id]]))
+        self.previously_seen_classes = np.unique(self.main_trainer.class_splits[:self.task_idx]).tolist()
+        self.seen_classes = np.unique(self.main_trainer.class_splits[:self.task_idx + 1]).tolist()
         self.writer = SummaryWriter(os.path.join(self.main_trainer.paths.logs_path, f'task_{self.task_idx}'))
 
     def train_on_batch(self, batch: Tuple[np.ndarray, np.ndarray]):
@@ -62,15 +63,15 @@ class MeRGAZSLTaskTrainer(TaskTrainer):
         self.optim['discr'].step()
         self.optim['discr'].zero_grad()
 
-        self.writer.add_scalar(f'Train/discr/discr_loss', discr_loss.item(), self.num_iters_done)
-        self.writer.add_scalar(f'Train/discr/cls_loss', cls_loss.item(), self.num_iters_done)
-        self.writer.add_scalar(f'Train/discr/grad_penalty', grad_penalty.item(), self.num_iters_done)
+        self.writer.add_scalar(f'discr/discr_loss', discr_loss.item(), self.num_iters_done)
+        self.writer.add_scalar(f'discr/cls_loss', cls_loss.item(), self.num_iters_done)
+        self.writer.add_scalar(f'discr/grad_penalty', grad_penalty.item(), self.num_iters_done)
 
         acc_on_real = (cls_pruned_logits_on_real.argmax(axis=1) == y).float().mean()
         acc_on_fake = (cls_pruned_logits_on_fake.argmax(axis=1) == y).float().mean()
 
-        self.writer.add_scalar(f'Train/discr/cls_acc_on_real', acc_on_real.item(), self.num_iters_done)
-        self.writer.add_scalar(f'Train/discr/cls_acc_on_fake', acc_on_fake.item(), self.num_iters_done)
+        self.writer.add_scalar(f'discr/cls_acc_on_real', acc_on_real.item(), self.num_iters_done)
+        self.writer.add_scalar(f'discr/cls_acc_on_fake', acc_on_fake.item(), self.num_iters_done)
 
     def generator_step(self, x: Tensor, y: Tensor):
         z = self.model.generator.sample_noise(x.size(0)).to(self.device_name)
@@ -95,9 +96,9 @@ class MeRGAZSLTaskTrainer(TaskTrainer):
         self.optim['gen'].step()
         self.optim['gen'].zero_grad()
 
-        self.writer.add_scalar(f'Train/gen/discr_loss', discr_loss.item(), self.num_iters_done)
-        self.writer.add_scalar(f'Train/gen/cls_loss', cls_loss.item(), self.num_iters_done)
-        self.writer.add_scalar(f'Train/gen/centroid_loss', centroid_loss.item(), self.num_iters_done)
+        self.writer.add_scalar(f'gen/discr_loss', discr_loss.item(), self.num_iters_done)
+        self.writer.add_scalar(f'gen/cls_loss', cls_loss.item(), self.num_iters_done)
+        self.writer.add_scalar(f'gen/centroid_loss', centroid_loss.item(), self.num_iters_done)
 
     def knowledge_distillation_step(self):
         z = self.model.generator.sample_noise(self.config.hp.model_config.distill_batch_size).to(self.device_name)
@@ -110,11 +111,11 @@ class MeRGAZSLTaskTrainer(TaskTrainer):
         self.optim['gen'].step()
         self.optim['gen'].zero_grad()
 
-        self.writer.add_scalar(f'Train/gen/knowledge_distillation_loss', loss.item(), self.num_iters_done)
+        self.writer.add_scalar(f'gen/knowledge_distillation_loss', loss.item(), self.num_iters_done)
 
     def classifier_trainer_step(self):
         # Randomly sampling classes
-        y = random.sample(self.seen_classes, self.config.hp.joint_clf_training_batch_size)
+        y = np.random.choice(self.seen_classes, size=self.config.hp.joint_clf_training_batch_size)
         y = torch.tensor(y).to(self.device_name).long()
 
         with torch.no_grad():
@@ -122,7 +123,7 @@ class MeRGAZSLTaskTrainer(TaskTrainer):
             x = self.model.generator(z, self.model.attrs[y])
 
         cls_logits = self.model.discriminator.run_cls_head(x)
-        seen_classes_output_mask = np.zeros(self.config.data.num_classes)
+        seen_classes_output_mask = np.zeros(self.config.data.num_classes).astype(bool)
         seen_classes_output_mask[self.seen_classes] = True
         pruned_logits = prune_logits(cls_logits, seen_classes_output_mask)
         loss = self.criterion(pruned_logits, y)
@@ -131,10 +132,13 @@ class MeRGAZSLTaskTrainer(TaskTrainer):
 
         self.optim['discr'].zero_grad()
         loss.backward()
+        if self.config.hp.has('clip_grad_norm'):
+            grad_norm = clip_grad_norm_(self.model.parameters(), self.config.hp.clip_grad_norm)
+            self.writer.add_scalar(f'cls/grad_norm', grad_norm.item(), self.num_iters_done)
         self.optim['discr'].step()
 
-        self.writer.add_scalar(f'Train/cls/loss', loss.item(), self.num_iters_done)
-        self.writer.add_scalar(f'Train/cls/acc', acc.item(), self.num_iters_done)
+        self.writer.add_scalar(f'cls/loss', loss.item(), self.num_iters_done)
+        self.writer.add_scalar(f'cls/acc', acc.item(), self.num_iters_done)
 
     def compute_centroid_loss(self, x_fake, y):
         groups = {}
