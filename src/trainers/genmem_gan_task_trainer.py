fix: proper GP calculation

src/trainers/genmem_gan_task_trainer.py
@@ -133,18 +133,31 @@ class GenMemGANTaskTrainer(TaskTrainer):
         return x, y
 
     def plot_samples(self):
-        y = np.arange(self.config.data.num_classes).repeat(self.config.plotting.num_samples_per_class)
-        z = torch.tensor(self.fixed_noise[:self.config.plotting.num_samples_per_class]).to(self.device_name)
+        classes = np.arange(self.config.data.num_classes).repeat(self.config.plotting.n_samples_per_class)
+        z = torch.tensor(self.fixed_noise[:self.config.plotting.n_samples_per_class]).to(self.device_name)
         z = z.repeat(self.config.data.num_classes, 1)
 
         with torch.no_grad():
-            x = self.model.generator(z, torch.tensor(y).to(self.device_name))
+            x = self.model.generator(z, torch.tensor(classes).to(self.device_name))
             x = x.cpu()
 
-        for i, (img, cls) in enumerate(zip(x, y)):
-            #img = ((img.permute(1, 2, 0).numpy() + 1) * (255 / 2)).astype(np.int32)
-            img = ((img.permute(1, 2, 0) + 1).numpy() * 255 / 2).astype(np.int32)
-            fig = plt.figure(figsize=(5, 5))
-            plt.imshow(img)
-            sample_idx = i % self.config.plotting.num_samples_per_class
-            self.writer.add_figure(f'Class_{cls}/sample_{sample_idx}', fig, self.num_iters_done)
+        for y in np.arange(self.config.data.num_classes):
+            imgs = x[y * self.config.plotting.n_samples_per_class: (y+1) * self.config.plotting.n_samples_per_class]
+            imgs = ((imgs.permute(0, 2, 3, 1).numpy() + 1) * 127.5).astype(int)
+            img_h, img_w = imgs.shape[1], imgs.shape[2]
+
+            n_rows = int(np.sqrt(self.config.plotting.n_samples_per_class))
+            n_cols = n_rows
+
+            assert n_rows * n_cols == self.config.plotting.n_samples_per_class
+
+            result = np.zeros((n_rows * img_h, n_cols * img_w, 3)).astype(int)
+
+            for i, img in enumerate(imgs):
+                h = img_h * (i // n_rows)
+                w = img_w * (i % n_cols)
+                result[h:h + img_h, w:w + img_w] = img
+
+            fig = plt.figure(figsize=(25, 25))
+            plt.imshow(result)
+            self.writer.add_figure(f'Class_{y}', fig, self.num_iters_done)

src/trainers/lll_trainer.py
@@ -97,8 +97,8 @@ class LLLTrainer(BaseTrainer):
             self.ds_test = awa.load_dataset(self.config.data.dir, split='test', target_shape=self.config.hp.img_target_shape)
             self.class_attributes = awa.load_class_attributes(self.config.data.dir)
         elif self.config.data.name == 'SVHN':
-            ds_train = SVHN(self.config.data.dir, split='train', transform=imagenet_normalization)
-            ds_test = SVHN(self.config.data.dir, split='test', transform=imagenet_normalization)
+            ds_train = SVHN(self.config.data.dir, split='train')
+            ds_test = SVHN(self.config.data.dir, split='test')
 
             ds_train_imgs = [(x / 127.5 - 1).astype(np.float32) for x in ds_train.data]
             ds_test_imgs = [(x / 127.5 - 1).astype(np.float32) for x in ds_test.data]

src/utils/losses.py
@@ -2,26 +2,27 @@ import torch
 from torch import Tensor, autograd
 
 
-def compute_gradient_penalty(discriminator, x_real, x_fake) -> Tensor:
+def compute_gradient_penalty(discriminator, x_real, x_fake):
     """
     Computes gradient penalty according to WGAN-GP paper
     """
     assert x_real.size() == x_fake.size()
 
-    eps = torch.rand(x_real.size(0)).to(x_real.device)
-    eps = eps.view(-1, *([1] * len(x_real.shape[1:]))) # Expanding dimensions
-    interpolations = eps * x_real + (1 - eps) * x_fake
+    shape = [x_real.size(0)] + [1] * (x_real.dim() - 1)
+    alpha = torch.rand(shape).to(x_real.device)
+    interpolations = x_real + alpha * (x_fake - x_real)
+
+    interpolations = interpolations.to(x_real.device)
     interpolations.requires_grad_(True)
     outputs = discriminator(interpolations)
-
     grads = autograd.grad(
-        outputs=outputs,
-        inputs=interpolations,
-        grad_outputs=torch.ones(outputs.size()).to(x_real.device),
-        retain_graph=True, create_graph=True, only_inputs=True
-    )[0]
+        outputs,
+        interpolations,
+        grad_outputs=torch.ones(outputs.size()).to(interpolations.device),
+        create_graph=True
+    )[0].view(interpolations.size(0), -1)
 
-    return ((grads.norm(2, dim=1) - 1) ** 2).mean()
+    return ((grads.norm(p=2, dim=1) - 1) ** 2).mean()
 
 
 def compute_kld_with_standard_gaussian(mean: Tensor, log_var: Tensor) -> Tensor: