9d307cfb4f943d091f5066f48d8a18c77d0c5cf8
Ivan Skorokhodov <iskorokhodov@gmail.com>
2020-03-20 20:43:10 +0300

    exp: experiments without slurm are tough :(

--- slurm/dem.sh
+++ slurm/dem.sh
@@ -49,6 +49,7 @@ CUDA_VISIBLE_DEVICES=0 python src/run.py -c icarl -d cub --experiments_dir cub-e
 # A-GEM
 CUDA_VISIBLE_DEVICES=1 python src/run.py -c agem -d cub --experiments_dir cub-experiments --config.hp.num_mem_samples_per_class 1 &
 CUDA_VISIBLE_DEVICES=2 python src/run.py -c agem -d cub --experiments_dir cub-experiments --config.hp.num_mem_samples_per_class 2 &
+CUDA_VISIBLE_DEVICES=3 python src/run.py -c agem -d cub --experiments_dir cub-experiments --config.hp.num_mem_samples_per_class 3 &
 CUDA_VISIBLE_DEVICES=3 python src/run.py -c agem -d cub --experiments_dir cub-experiments --config.hp.num_mem_samples_per_class 5 &
 
 
@@ -61,28 +62,32 @@ CUDA_VISIBLE_DEVICES=0 python src/run.py -c dem -d tiny_imagenet --experiments_d
 CUDA_VISIBLE_DEVICES=0 python src/run.py -c dem -d tiny_imagenet --experiments_dir tiny_imagenet-experiments --config.hp.memory.num_samples_per_class 32 --config.hp.memory.downsample_size 16 --config.hp.lowres_training.logits_matching_loss_coef 1 2> logs/tiny_imagenet-dem-16-32.log &
 
 CUDA_VISIBLE_DEVICES=1 python src/run.py -c dem -d tiny_imagenet --experiments_dir tiny_imagenet-experiments --config.hp.memory.num_samples_per_class 1 --config.hp.memory.downsample_size 32 --config.hp.lowres_training.logits_matching_loss_coef 1 2> logs/tiny_imagenet-dem-32-1.log &
+CUDA_VISIBLE_DEVICES=1 python src/run.py -c dem -d tiny_imagenet --experiments_dir tiny_imagenet-experiments --config.hp.memory.num_samples_per_class 2 --config.hp.memory.downsample_size 32 --config.hp.lowres_training.logits_matching_loss_coef 1 2> logs/tiny_imagenet-dem-32-2.log &
 CUDA_VISIBLE_DEVICES=1 python src/run.py -c dem -d tiny_imagenet --experiments_dir tiny_imagenet-experiments --config.hp.memory.num_samples_per_class 4 --config.hp.memory.downsample_size 32 --config.hp.lowres_training.logits_matching_loss_coef 1 2> logs/tiny_imagenet-dem-32-4.log &
 CUDA_VISIBLE_DEVICES=1 python src/run.py -c dem -d tiny_imagenet --experiments_dir tiny_imagenet-experiments --config.hp.memory.num_samples_per_class 8 --config.hp.memory.downsample_size 32 --config.hp.lowres_training.logits_matching_loss_coef 1 2> logs/tiny_imagenet-dem-32-8.log &
 CUDA_VISIBLE_DEVICES=1 python src/run.py -c dem -d tiny_imagenet --experiments_dir tiny_imagenet-experiments --config.hp.memory.num_samples_per_class 16 --config.hp.memory.downsample_size 32 --config.hp.lowres_training.logits_matching_loss_coef 1 2> logs/tiny_imagenet-dem-32-16.log &
 CUDA_VISIBLE_DEVICES=1 python src/run.py -c dem -d tiny_imagenet --experiments_dir tiny_imagenet-experiments --config.hp.memory.num_samples_per_class 32 --config.hp.memory.downsample_size 32 --config.hp.lowres_training.logits_matching_loss_coef 1 2> logs/tiny_imagenet-dem-32-32.log &
 
 # Simple baselines
-CUDA_VISIBLE_DEVICES=3 python src/run.py -c basic -d tiny_imagenet --experiments_dir tiny_imagenet-experiments 2> logs/tiny_imagenet-basic.log .log &
-CUDA_VISIBLE_DEVICES=0 python src/run.py -c joint -d tiny_imagenet --experiments_dir tiny_imagenet-experiments 2> logs/tiny_imagenet-joint.log .log &
+CUDA_VISIBLE_DEVICES=2 python src/run.py -c basic -d tiny_imagenet --experiments_dir tiny_imagenet-experiments 2> logs/tiny_imagenet-basic.log .log &
+CUDA_VISIBLE_DEVICES=3 python src/run.py -c joint -d tiny_imagenet --experiments_dir tiny_imagenet-experiments 2> logs/tiny_imagenet-joint.log .log &
 
 # Vanilla EM
 CUDA_VISIBLE_DEVICES=1 python src/run.py -c dem -d tiny_imagenet --experiments_dir tiny_imagenet-experiments --config.hp.memory.num_samples_per_class 1 --config.hp.memory.downsample_size 64 2> logs/tiny_imagenet-dem-64-1.log &
-CUDA_VISIBLE_DEVICES=3 python src/run.py -c dem -d tiny_imagenet --experiments_dir tiny_imagenet-experiments --config.hp.memory.num_samples_per_class 2 --config.hp.memory.downsample_size 64 2> logs/tiny_imagenet-dem-64-2.log &
+CUDA_VISIBLE_DEVICES=4 python src/run.py -c dem -d tiny_imagenet --experiments_dir tiny_imagenet-experiments --config.hp.memory.num_samples_per_class 2 --config.hp.memory.downsample_size 64 2> logs/tiny_imagenet-dem-64-2.log &
+CUDA_VISIBLE_DEVICES=4 python src/run.py -c dem -d tiny_imagenet --experiments_dir tiny_imagenet-experiments --config.hp.memory.num_samples_per_class 4 --config.hp.memory.downsample_size 64 2> logs/tiny_imagenet-dem-64-4.log &
 CUDA_VISIBLE_DEVICES=7 python src/run.py -c dem -d tiny_imagenet --experiments_dir tiny_imagenet-experiments --config.hp.memory.num_samples_per_class 5 --config.hp.memory.downsample_size 64 2> logs/tiny_imagenet-dem-64-5.log &
 
 # iCARL
 CUDA_VISIBLE_DEVICES=2 python src/run.py -c icarl -d tiny_imagenet --experiments_dir tiny_imagenet-experiments --config.hp.memory.max_size 200 2> logs/tiny_imagenet-icarl-200.log &
 CUDA_VISIBLE_DEVICES=8 python src/run.py -c icarl -d tiny_imagenet --experiments_dir tiny_imagenet-experiments --config.hp.memory.max_size 400 2> logs/tiny_imagenet-icarl-400.log &
+CUDA_VISIBLE_DEVICES=6 python src/run.py -c icarl -d tiny_imagenet --experiments_dir tiny_imagenet-experiments --config.hp.memory.max_size 600 2> logs/tiny_imagenet-icarl-600.log &
 CUDA_VISIBLE_DEVICES=9 python src/run.py -c icarl -d tiny_imagenet --experiments_dir tiny_imagenet-experiments --config.hp.memory.max_size 1000 2> logs/tiny_imagenet-icarl-1000.log &
 
 # A-GEM
 CUDA_VISIBLE_DEVICES=4 python src/run.py -c agem -d tiny_imagenet --experiments_dir tiny_imagenet-experiments --config.hp.num_mem_samples_per_class 1 2> logs/tiny_imagenet-agem-1.log &
 CUDA_VISIBLE_DEVICES=5 python src/run.py -c agem -d tiny_imagenet --experiments_dir tiny_imagenet-experiments --config.hp.num_mem_samples_per_class 2 2> logs/tiny_imagenet-agem-2.log &
+CUDA_VISIBLE_DEVICES=8 python src/run.py -c agem -d tiny_imagenet --experiments_dir tiny_imagenet-experiments --config.hp.num_mem_samples_per_class 3 2> logs/tiny_imagenet-agem-3.log &
 CUDA_VISIBLE_DEVICES=6 python src/run.py -c agem -d tiny_imagenet --experiments_dir tiny_imagenet-experiments --config.hp.num_mem_samples_per_class 5 2> logs/tiny_imagenet-agem-5.log &
 
 ## AwA
@@ -111,17 +116,17 @@ CUDA_VISIBLE_DEVICES=3 python src/run.py -c joint -d awa --experiments_dir awa-e
 
 # Vanilla EM
 CUDA_VISIBLE_DEVICES=1 python src/run.py -c dem -d awa --experiments_dir awa-experiments --config.hp.memory.num_samples_per_class 1 --config.hp.memory.downsample_size 256 &
-CUDA_VISIBLE_DEVICES=1 python src/run.py -c dem -d awa --experiments_dir awa-experiments --config.hp.memory.num_samples_per_class 2 --config.hp.memory.downsample_size 256 &
+CUDA_VISIBLE_DEVICES=0 python src/run.py -c dem -d awa --experiments_dir awa-experiments --config.hp.memory.num_samples_per_class 3 --config.hp.memory.downsample_size 256 2> logs/awa_dem_256_3.log &
 CUDA_VISIBLE_DEVICES=1 python src/run.py -c dem -d awa --experiments_dir awa-experiments --config.hp.memory.num_samples_per_class 5 --config.hp.memory.downsample_size 256 &
 
 # iCARL
 CUDA_VISIBLE_DEVICES=6 python src/run.py -c icarl -d awa --experiments_dir awa-experiments --config.hp.memory.max_size 200 &
-CUDA_VISIBLE_DEVICES=6 python src/run.py -c icarl -d awa --experiments_dir awa-experiments --config.hp.memory.max_size 400 &
+CUDA_VISIBLE_DEVICES=6 python src/run.py -c icarl -d awa --experiments_dir awa-experiments --config.hp.memory.max_size 600 2> logs/awa_icarl_600.log &
 CUDA_VISIBLE_DEVICES=6 python src/run.py -c icarl -d awa --experiments_dir awa-experiments --config.hp.memory.max_size 1000 &
 
 # A-GEM
 CUDA_VISIBLE_DEVICES=7 python src/run.py -c agem -d awa --experiments_dir awa-experiments --config.hp.num_mem_samples_per_class 1 &
-CUDA_VISIBLE_DEVICES=7 python src/run.py -c agem -d awa --experiments_dir awa-experiments --config.hp.num_mem_samples_per_class 2 &
+CUDA_VISIBLE_DEVICES=7 python src/run.py -c agem -d awa --experiments_dir awa-experiments --config.hp.num_mem_samples_per_class 2 2> logs/awa_agem_2.log &
 CUDA_VISIBLE_DEVICES=7 python src/run.py -c agem -d awa --experiments_dir awa-experiments --config.hp.num_mem_samples_per_class 5 &
 
 
@@ -132,3 +137,9 @@ CUDA_VISIBLE_DEVICES=2 python src/run.py -c dem -d tiny_imagenet --experiments_d
 CUDA_VISIBLE_DEVICES=3 python src/run.py -c dem -d tiny_imagenet --experiments_dir lm-ablation-experiments --config.hp.memory.num_samples_per_class 8 --config.hp.memory.downsample_size 32 --config.hp.lowres_training.logits_matching_loss_coef 1 2> logs/lm-ablation-tiny_imagenet-lm-coef-1.log &
 CUDA_VISIBLE_DEVICES=4 python src/run.py -c dem -d cub --experiments_dir lm-ablation-experiments --config.hp.memory.num_samples_per_class 8 --config.hp.memory.downsample_size 64 --config.hp.lowres_training.logits_matching_loss_coef 0 2> logs/lm-ablation-cub-lm-coef-0.log &
 CUDA_VISIBLE_DEVICES=5 python src/run.py -c dem -d cub --experiments_dir lm-ablation-experiments --config.hp.memory.num_samples_per_class 8 --config.hp.memory.downsample_size 64 --config.hp.lowres_training.logits_matching_loss_coef 1 2> logs/lm-ablation-cub-lm-coef-1.log &
+
+CUDA_VISIBLE_DEVICES=4 python src/run.py -c dem -d cub --experiments_dir cub-experiments --config.hp.memory.num_samples_per_class 8 --config.hp.memory.downsample_size 128 --config.hp.lowres_training.logits_matching_loss_coef 0
+CUDA_VISIBLE_DEVICES=5 python src/run.py -c dem -d cub --experiments_dir cub-experiments --config.hp.memory.num_samples_per_class 8 --config.hp.memory.downsample_size 128 --config.hp.lowres_training.logits_matching_loss_coef 1
+
+CUDA_VISIBLE_DEVICES=7 python src/run.py -c dem -d cub --experiments_dir cub-experiments --config.hp.memory.num_samples_per_class 4 --config.hp.memory.downsample_size 128 --config.hp.lowres_training.logits_matching_loss_coef 0
+CUDA_VISIBLE_DEVICES=8 python src/run.py -c dem -d cub --experiments_dir cub-experiments --config.hp.memory.num_samples_per_class 4 --config.hp.memory.downsample_size 128 --config.hp.lowres_training.logits_matching_loss_coef 1
