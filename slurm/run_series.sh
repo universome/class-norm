117fe6aef92b964da317dd13e4b68ae8b4173e05
Ivan Skorokhodov <iskorokhodov@gmail.com>
2020-04-02 20:24:59 +0300

    exp: CZSL with generated prototypes

--- slurm/run_series.sh
+++ slurm/run_series.sh
@@ -6,69 +6,47 @@ experiments_dir="multi_proto_random"
 mkdir -p $experiments_dir
 config="multi_proto"
 dataset=cub
+case "$dataset" in
+    cub) mem=64G ;;
+    awa) mem=256G ;;
+    *) mem=128G ;;
+esac
 
 for (( random_seed=1; random_seed<=num_runs; random_seed++ )); do
-    for num_prototypes in 1 10; do
-        for num_test_prototypes in 1 10; do
-            for transform_layers in {64} {64-128}; do
-                for scale_value in 10; do
-                    for std in 0 0.1; do
-                        case "$dataset" in
-                            cub) mem=64G ;;
-                            awa) mem=256G ;;
-                            *) mem=128G ;;
-                        esac
-
-                        # case "$num_prototypes" in
-                        #     10) scale_value=5 ;;
-                        #     *) scale_value=10 ;;
-                        # esac
-
-                        cli_args=$(echo "-c $config -d $dataset --experiments_dir $experiments_dir -s $random_seed" \
-                                "--config.hp.head.num_prototypes $num_prototypes" \
-                                "--config.hp.head.num_test_prototypes $num_test_prototypes" \
-                                "--config.hp.head.context.transform_layers $transform_layers" \
-                                "--config.hp.head.scale_value $scale_value" \
-                                "--config.hp.head.context.std $std")
-                        echo "$cli_args"
-                        # sbatch --mem "$mem" --export=ALL,cli_args="$cli_args" slurm/slurm_lll_job.sh;
+    for noise_transform_layers in {128} {128-256-128}; do
+        for attrs_transform_layers in {312} {312-256}; do
+            for after_fuse_transform_layers in {512} {256-512}; do
+                for same_for_each_class in true false; do
+                    for fusing_type in concat full_mult_int; do
+                        for std in 0.1 0.5; do
+                            cli_args=$(echo "-c $config -d $dataset --experiments_dir $experiments_dir -s $random_seed" \
+                                    "--config.hp.head.num_prototypes 10" \
+                                    "--config.hp.head.num_test_prototypes 25" \
+                                    "--config.hp.head.noise.transform_layers $noise_transform_layers" \
+                                    "--config.hp.head.noise.same_for_each_class $same_for_each_class" \
+                                    "--config.hp.head.fusing_type $fusing_type" \
+                                    "--config.hp.head.attrs_transform_layers $attrs_transform_layers" \
+                                    "--config.hp.head.after_fuse_transform_layers $after_fuse_transform_layers" \
+                                    "--config.hp.head.noise.std $std")
+                            # echo "$cli_args"
+                            sbatch --mem "$mem" --export=ALL,cli_args="$cli_args" slurm/slurm_lll_job.sh;
+                        done
                     done
                 done
             done
         done
     done
+
+    # Baseline
+    cli_args=$(echo "-c $config -d $dataset --experiments_dir $experiments_dir -s $random_seed" \
+            "--config.hp.head.num_prototypes 1" \
+            "--config.hp.head.num_test_prototypes 1" \
+            "--config.hp.head.noise.transform_layers {128}" \
+            "--config.hp.head.noise.same_for_each_class true" \
+            "--config.hp.head.fusing_type concat" \
+            "--config.hp.head.attrs_transform_layers {312}" \
+            "--config.hp.head.after_fuse_transform_layers {512}" \
+            "--config.hp.head.noise.std 0.0")
+    # echo "$cli_args"
+    sbatch --mem "$mem" --export=ALL,cli_args="$cli_args" slurm/slurm_lll_job.sh;
 done
-
-# experiments_dir="multi_headed"
-# mkdir -p $experiments_dir
-# config="multi_proto"
-# dataset=cub
-
-# for (( random_seed=1; random_seed<=num_runs; random_seed++ )); do
-#     for num_prototypes in 1 10 25; do
-#         for embedder_hidden_layers in {128} {128-256} {128-256-256}; do
-#             for use_final_activation in true false; do
-#                 for scale_value in 5 10 15; do
-#                     case "$dataset" in
-#                         cub) mem=64G ;;
-#                         awa) mem=256G ;;
-#                         *) mem=128G ;;
-#                     esac
-
-#                     # case "$num_prototypes" in
-#                     #     10) scale_value=5 ;;
-#                     #     *) scale_value=10 ;;
-#                     # esac
-
-#                     cli_args=$(echo "-c $config -d $dataset --experiments_dir $experiments_dir -s $random_seed" \
-#                             "--config.hp.head.num_prototypes $num_prototypes" \
-#                             "--config.hp.head.embedder_hidden_layers $embedder_hidden_layers" \
-#                             "--config.hp.head.scale_value $scale_value" \
-#                             "--config.hp.head.use_final_activation $use_final_activation")
-#                     # echo "$cli_args"
-#                     sbatch --mem "$mem" --export=ALL,cli_args="$cli_args" slurm/slurm_lll_job.sh;
-#                 done
-#             done
-#         done
-#     done
-# done
--- slurm/slurm_lll_job.sh
+++ slurm/slurm_lll_job.sh
@@ -5,5 +5,5 @@
 #SBATCH --cpus-per-task 4
 #SBATCH -o logs/output-%j.out
 
-cd /home/skoroki/zslll-master-2
+cd /home/skoroki/zslll-master
 python src/run.py $cli_args
