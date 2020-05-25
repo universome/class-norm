59927c9ee2629d888ce8aca7de6d7cbd8ab2dd33
Ivan Skorokhodov <iskorokhodov@gmail.com>
2020-05-25 22:13:36 +0300

    log: check that a GPU is available

--- slurm/slurm_lll_job.sh
+++ slurm/slurm_lll_job.sh
@@ -8,5 +8,9 @@ mkdir -p /tmp/skoroki/czsl/data
 cp "/ibex/scratch/skoroki/datasets/${dataset_full_name}.tar.gz" /tmp/skoroki/czsl/data
 tar zxvf "/tmp/skoroki/czsl/data/${dataset_full_name}.tar.gz" -C /tmp/skoroki/czsl/data
 
+echo "`gpustat`"
+echo "`nvidia-smi`"
+echo "CLI args: $cli_args"
+
 cd /home/skoroki/zslll-master
 python src/run.py $cli_args
